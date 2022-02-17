fc287018c3a4a18ccf9b31b1de0f092570c1a50d
Mariyan Karakiev <mariqnkarakiev@gmail.com>
2022-02-17 18:10:36 +0200

    Removed private constants

--- C# Web Basics/BasicWebServer/BasicWebServer.Server/Controllers/HomeController.cs
+++ C# Web Basics/BasicWebServer/BasicWebServer.Server/Controllers/HomeController.cs
@@ -11,14 +11,6 @@ namespace BasicWebServer.Server.Controllers
 {
     public class HomeController : Controller
     {
-        private const string HtmlForm = @"<form action = '/HTML' method='POST'>
-Name: <input type='text' name='Name'/>
-Age: <input type='number' name ='Age'/>
-<input type='submit' value ='Save'/>
-</form>";
-        private const string DownloadForm = @"<form action='/Content' method='POST'>
-<input type='submit' value ='Download Sites Content' />
-</form>";
         private const string FileName = "test.pdf";
 
         public HomeController(Request request)
@@ -57,20 +49,19 @@ Age: <input type='number' name ='Age'/>
 
         public Response Index() => Text("Hello from the server!");
         public Response Redirect() => Redirect("https://softuni.org/");
-        public Response Html() => Html(HtmlForm);
+        public Response Html() => View();
         public Response HtmlFormPost()
         {
-            string formData = "";
+           var formData = new StringBuilder();
 
             foreach (var (key, value) in Request.Form)
             {
-                formData += $"{key} - {value}";
-                formData += Environment.NewLine;
+                formData.AppendLine($"{key} - {value}");             
             }
 
-            return Text(formData);
+            return Text(formData.ToString());
         }
-        public Response Content() => Html(DownloadForm);
+        public Response Content() => View();
         public Response DownloadContent()
         {
             //DownloadSitesAsTextFile(
